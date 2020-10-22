Test and fix an issue with L computation git status!

src/models/CostModel.py
@@ -162,9 +162,9 @@ class LogisticModel(ACostModel):
         start = time.time()
         # Handle the case if we are using a scipy sparse matrix.
         if (isinstance(self.X, sp.csc.csc_matrix)):
-            L = sp.linalg.norm(self.X.T.dot(self.X)) / (4 * n_sample) + self.regularization.regularization_rate
+            L = sp.linalg.norm(self.X.T.dot(self.X)) / (4 * n_sample)
         else:
-            L = (torch.norm(self.X.T.mm(self.X), p=2) / (4 * n_sample)).item() + self.regularization.regularization_rate
+            L = (torch.norm(self.X.T.mm(self.X), p=2) / (4 * n_sample)).item()
         end = time.time()
         self.lips_times += (end - start)
         return L
@@ -207,8 +207,7 @@ class RMSEModel(ACostModel):
     def lips(self):
         n_sample = self.X.shape[0]
         start = time.time()
-        L = (2 * torch.norm(self.X.T.mm(self.X),
-                            p=2) / n_sample).item() + 2 * self.regularization.regularization_rate
+        L = (2 * torch.norm(self.X.T.mm(self.X), p=2) / n_sample).item()
         end = time.time()
         self.lips_times += (end - start)
         return L

test/testParameters.py
@@ -5,9 +5,21 @@ Test to verify that predefined parameters are correctly set up.
 """
 
 import unittest
+
+import torch
+
 from src.machinery.Parameters import *
-from src.machinery.PredefinedParameters import BiQSGD
+from src.machinery.PredefinedParameters import BiQSGD, Diana, Artemis, Qsgd, VanillaSGD
+from src.models.CostModel import RMSEModel
 from src.utils.Constants import DIM, NB_EPOCH, NB_DEVICES
+
+X = torch.tensor([[-1,  0,  1], [1, 1, 1], [0, 0, 1]], dtype=torch.float64)
+w_true = torch.tensor([1,  2, -1], dtype=torch.float64)
+Y_reg = X.mv(w_true)
+
+
+def step_formula_for_test(it, L, omega, N): return 1 / (L * sqrt(it))
+
 
 class ParametersTest(unittest.TestCase):
     def setUp(self):
@@ -15,14 +27,15 @@ class ParametersTest(unittest.TestCase):
 
     def test_parameters_instantiation_without_arguments(self):
         """All arguments of parameters class have default values. Check that this values are correct."""
-        params = Parameters()
+        params = Parameters(cost_models=RMSEModel(X, Y_reg), step_formula=step_formula_for_test)
 
-        self.assertIs(type(params.cost_model), RMSEModel)
+        self.assertIs(type(params.cost_models), type(RMSEModel(X, Y_reg)))
         self.assertEqual(params.federated, False)
         self.assertEqual(params.n_dimensions, DIM)
         self.assertEqual(params.nb_devices, NB_DEVICES)
         self.assertEqual(params.batch_size, 1)
-        self.assertEqual(params.step_formula.__code__.co_code, (lambda it, L, omega, N :1 / (L * sqrt(it))).__code__.co_code)
+        self.assertEqual(
+            params.step_formula.__code__.co_code, step_formula_for_test.__code__.co_code)
         self.assertEqual(params.nb_epoch, NB_EPOCH)
         self.assertEqual(params.regularization_rate, 0)
         self.assertEqual(params.momentum, 0)
@@ -38,50 +51,53 @@ class ParametersTest(unittest.TestCase):
 
     def test_StochasticSingleCompressionWithMemory_iscorrect(self):
         n_dimensions, nb_devices, quantization_param = 10, 10, 1
-        params = Diana().define(n_dimensions, nb_devices, quantization_param)
+        params = Diana().define(
+            cost_models=RMSEModel(X, Y_reg), n_dimensions=n_dimensions, nb_devices=nb_devices,
+            quantization_param=quantization_param, step_formula=step_formula_for_test)
         self.assertEqual(params.n_dimensions, n_dimensions)
         self.assertEqual(params.nb_devices, nb_devices)
         self.assertEqual(params.nb_epoch, NB_EPOCH)
-        self.assertEqual(params.step_formula.__code__.co_code,
-                         (lambda it, L, omega, N: 1 / (L * sqrt(it))).__code__.co_code)
+        self.assertEqual(params.step_formula.__code__.co_code, step_formula_for_test.__code__.co_code)
         self.assertEqual(params.quantization_param, quantization_param)
         self.assertEqual(params.momentum, 0)
         self.assertEqual(params.verbose, False)
         self.assertEqual(params.stochastic, True)
-        self.assertIs(type(params.cost_model), RMSEModel)
+        self.assertIs(type(params.cost_models), type(RMSEModel(X, Y_reg)))
         self.assertEqual(params.use_averaging, False)
         self.assertEqual(params.bidirectional, False)
 
     def test_StochasticSingleCompressionWithoutMemory_iscorrect(self):
         n_dimensions, nb_devices, quantization_param = 10, 10, 1
-        params = Qsgd().define(n_dimensions, nb_devices, quantization_param)
+        params = Qsgd().define(
+            cost_models=RMSEModel(X, Y_reg), n_dimensions=n_dimensions, nb_devices=nb_devices,
+            quantization_param=quantization_param, step_formula=step_formula_for_test)
         self.assertEqual(params.n_dimensions, n_dimensions)
         self.assertEqual(params.nb_devices, nb_devices)
         self.assertEqual(params.nb_epoch, NB_EPOCH)
-        self.assertEqual(params.step_formula.__code__.co_code,
-                         (lambda it, L, omega, N: 1 / (L * sqrt(it))).__code__.co_code)
+        self.assertEqual(params.step_formula.__code__.co_code, step_formula_for_test.__code__.co_code)
         self.assertEqual(params.quantization_param, quantization_param)
         self.assertEqual(params.momentum, 0)
         self.assertEqual(params.learning_rate, 0)
         self.assertEqual(params.verbose, False)
         self.assertEqual(params.stochastic, True)
-        self.assertIs(type(params.cost_model), RMSEModel)
+        self.assertIs(type(params.cost_models), type(RMSEModel(X, Y_reg)))
         self.assertEqual(params.use_averaging, False)
         self.assertEqual(params.bidirectional, False)
 
     def test_StochasticDoubleGradientsCompressionWithMemory_iscorrect(self):
         n_dimensions, nb_devices, quantization_param = 10, 10, 1
-        params = Artemis().define(n_dimensions, nb_devices, quantization_param)
+        params = Artemis().define(
+            cost_models=RMSEModel(X, Y_reg), n_dimensions=n_dimensions, nb_devices=nb_devices,
+            quantization_param=quantization_param, step_formula=step_formula_for_test)
         self.assertEqual(params.n_dimensions, n_dimensions)
         self.assertEqual(params.nb_devices, nb_devices)
         self.assertEqual(params.nb_epoch, NB_EPOCH)
-        self.assertEqual(params.step_formula.__code__.co_code,
-                         (lambda it, L, omega, N: 1 / (L * sqrt(it))).__code__.co_code)
+        self.assertEqual(params.step_formula.__code__.co_code, step_formula_for_test.__code__.co_code)
         self.assertEqual(params.quantization_param, quantization_param)
         self.assertEqual(params.momentum, 0)
         self.assertEqual(params.verbose, False)
         self.assertEqual(params.stochastic, True)
-        self.assertIs(type(params.cost_model), RMSEModel)
+        self.assertIs(type(params.cost_models), type(RMSEModel(X, Y_reg)))
         self.assertEqual(params.use_averaging, False)
         self.assertEqual(params.bidirectional, True)
         self.assertEqual(params.double_use_memory, False)
@@ -89,17 +105,18 @@ class ParametersTest(unittest.TestCase):
 
     def test_StochasticDoubleGradientsCompressionWithoutMemory_iscorrect(self):
         n_dimensions, nb_devices, quantization_param = 10, 10, 1
-        params = BiQSGD().define(n_dimensions, nb_devices, quantization_param)
+        params = BiQSGD().define(
+            cost_models=RMSEModel(X, Y_reg), n_dimensions=n_dimensions, nb_devices=nb_devices,
+            quantization_param=quantization_param, step_formula=step_formula_for_test)
         self.assertEqual(params.n_dimensions, n_dimensions)
         self.assertEqual(params.nb_devices, nb_devices)
         self.assertEqual(params.nb_epoch, NB_EPOCH)
-        self.assertEqual(params.step_formula.__code__.co_code,
-                         (lambda it, L, omega, N: 1 / (L * sqrt(it))).__code__.co_code)
+        self.assertEqual(params.step_formula.__code__.co_code, step_formula_for_test.__code__.co_code)
         self.assertEqual(params.quantization_param, quantization_param)
         self.assertEqual(params.momentum, 0)
         self.assertEqual(params.verbose, False)
         self.assertEqual(params.stochastic, True)
-        self.assertIs(type(params.cost_model), RMSEModel)
+        self.assertIs(type(params.cost_models), type(RMSEModel(X, Y_reg)))
         self.assertEqual(params.use_averaging, False)
         self.assertEqual(params.bidirectional, True)
         self.assertEqual(params.double_use_memory, False)