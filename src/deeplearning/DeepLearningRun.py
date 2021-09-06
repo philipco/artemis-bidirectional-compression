e3dcba595f4c2849fe1ad90e0f6e25d00a6a8aaf
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2021-09-06 15:39:10 +0200

    Add updated version of RunDL.

--- src/deeplearning/DeepLearningRun.py
+++ src/deeplearning/DeepLearningRun.py
@@ -1,6 +1,8 @@
 """
 Created by Philippenko, 27th April 2021.
 """
+import numpy as np
+
 from src.deeplearning.DLParameters import DLParameters
 
 
@@ -14,7 +16,20 @@ class DeepLearningRun:
         self.test_accuracies = []
         self.theoretical_nb_bits = []
 
+        self.best_val_loss = np.inf
+        self.epoch = parameters.nb_epoch
+
     def update_run(self, train_loss, test_loss, test_acc):
         self.train_losses.append(train_loss)
         self.test_losses.append(test_loss)
         self.test_accuracies.append(test_acc)
+
+    def there_is_nan(self):
+        print("There is NaN in output values, stopping.")
+        # Completing values to reach the given number of epoch.
+        self.train_losses = self.train_losses + [self.train_losses[-1] for i in
+                                               range(self.parameters.nb_epoch - len(self.train_losses) + 1)]
+        self.test_losses = self.test_losses \
+                           + [self.test_losses[-1] for i in range(self.parameters.nb_epoch - len(self.test_losses) + 1)]
+        self.test_accuracies = self.test_accuracies \
+                               + [self.test_accuracies[-1] for i in range(self.parameters.nb_epoch - len(self.test_accuracies) + 1)]
